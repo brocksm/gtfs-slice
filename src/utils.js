bbde55cc35c234b999c46c33ed93d8e7af2c9d38
brocksm <brocks.mike@gmail.com>
2021-06-13 11:16:27 -0700

    helper functions in common utility file
    - unique identifies and filters an array or object's key:values to their unique occurrences
    - choose finds all combinations of k items in an array of values

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+module.exports = {
+  unique: ({arr, key}) => {
+    let seen = {};
+    return arr.filter(function(item) {
+      let accessor = key ? item[key] : item,
+          result = seen.hasOwnProperty(accessor) ? false : (seen[accessor] = true);			    
+      return result;
+    }).map(arr => key ? arr[key] : arr)
+  },
+  choose: ({arr, k, prefix=[]}) => {
+    if (k == 0) return [prefix];
+  
+    return arr.flatMap((v, i) =>
+      choose(arr.slice(i+1), k-1, [...prefix, v])
+    );
+  }
+} 
