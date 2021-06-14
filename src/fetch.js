248ee5a1b2af89e337d938d64851c44a2f018bcb
brocksm <brocks.mike@gmail.com>
2021-06-13 19:50:11 -0700

    add fetch.js
    download and unzip GTFS files from supplied URL

---
+++ src/fetch.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+const unzip = require('adm-zip');
+const fs = require('fs');
+      
+module.exports = function (url) {
+  return fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      console.error(`Error downloading GTFS files from ${url} (HTTP response ${response.statusText})`)
+      process.exit(1);
+    }
+    else {return response.buffer();}
+  })		
+  .then((buffer) => {fs.writeFileSync(`${filename}.zip`, buffer);})
+  .then(() => {new unzip(`${filename}.zip`).extractAllTo(directory);});
+}
