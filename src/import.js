7942f62a109d24aed6f526f0dc5d6c583a79aed5
brocksm <brocks.mike@gmail.com>
2021-07-22 21:26:07 -0500

    update import.js & stream.js
    - remove directory as argument to stream.js
    - import readline to overwrite import statements for each table to same line
    - remove check(typeof()
    - reduce batch size from 5k to 1k

--- src/import.js
+++ src/import.js
@@ -4,14 +4,12 @@ module.exports = async function (tables, database) {
   for (const table of tables) {
     const tableName = table['name'];
     const schema = table['schema'];
-    const columnNamesAndTypes = schema.map((c) => {
-      c.name + ' ' + c.type + ' CHECK(typeof(' +
-      c.name + ') = ' + c.type + ')'
-    }).join(', ');
+    const columnNamesAndTypes = schema.map(c => 
+      c.name + ' ' + c.type).join(', ');
     const columns = schema.map(v => v.name).join(', ');
     const params = schema.map(v => '@' + v.name).join(', ');
     const constraints = schema.filter(v => v.constraint).map(v => v.name).join(', ');
-  
+
     database.prepare(
       `CREATE TABLE IF NOT EXISTS ${tableName} 
        (${columnNamesAndTypes}, UNIQUE(${constraints}) ON CONFLICT IGNORE)`
@@ -22,6 +20,6 @@ module.exports = async function (tables, database) {
       for (const row of rows) insertRow.run(row);
     });
     
-    await stream(table, insertRows, 5000);
+    await stream(table, insertRows, 1000);
   }
 }
--- src/stream.js
+++ src/stream.js
@@ -1,9 +1,10 @@
 const csv = require('csv-parser');
+const readline = require('readline');
 
 module.exports = function (table, callback, bufferSize) {
   let buffer = [],
       rows = 0,
-      readstream = fs.createReadStream(path.resolve(`${directory}${table.baseFilename}.txt`));
+      readstream = fs.createReadStream(path.resolve(`${'./'}${table.name}.txt`));
 
   return new Promise(function(resolve) {
     readstream
@@ -18,12 +19,17 @@ module.exports = function (table, callback, bufferSize) {
           callback(buffer);
           buffer = [];
           rows += bufferSize;
+          readline.clearLine(process.stdout, 0);
+          readline.cursorTo(process.stdout, 0);
+          process.stdout.write(`imported ${rows} rows into ${table['name']}`);
         }	
       })
       .on('end', () => {
         callback(buffer);
         rows += buffer.length;
-        console.log(`imported ${rows} rows into ${table['name']}`);
+        readline.clearLine(process.stdout, 0);
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write(`imported ${rows} rows into ${table['name']}\n`);
         resolve();
       });
   }).catch((error) => {console.error(error)})	
